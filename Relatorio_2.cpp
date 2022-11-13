a7fb844bdfee6c910d600ea807178df6c810c86c
VonErlenmeyer <ismaelture@gmail.com>
2022-11-13 01:51:15 +0000

    Gauss sem Pivot

--- Relatorio_2.cpp
+++ Relatorio_2.cpp
@@ -2,30 +2,40 @@
 #include <cmath>
 using namespace std;
 
-void inverse_sub(double (*matrix)[][3], double (*vector_b)[3], int n);
+void inverseSub(double (*matrix)[][3], double (*vector_b)[3], int n);
+void gaussNoPivot(double (*matrix)[][3], double (*vector_b)[3], int n);
 
 int main(void){
 
 double matrix[3][3]={{1,-1,-1},{0,2,-1},{0,0,-1}};
 double vector_b[3]={0,-2,-7};
 int n=3;
-inverse_sub(&matrix, &vector_b, n);
 
-matrix[2][0]=-2;
-inverse_sub(&matrix, &vector_b, n);
+cout << "1. Inversa, R3=0" << endl;
+inverseSub(&matrix, &vector_b, n);
+
+matrix[2][0]= -2;
+cout << "1. Inversa, R3=2" << endl;
+inverseSub(&matrix, &vector_b, n);
+
+cout << "Gauss R3=2" << endl;
+gaussNoPivot(&matrix, &vector_b, n);
+
+matrix[1][1]=0;
+cout << "Gauss R2=0" << endl;
+gaussNoPivot(&matrix, &vector_b, n);
 
 return 0;
 }
 
 
-void inverse_sub(double (*matrix)[][3], double (*vector_b)[3], int n){
+void inverseSub(double (*matrix)[][3], double (*vector_b)[3], int n){
 
 int j, i;
 double sum=0;
 double vector_x[3]={0};
 
 vector_x[n-1] = ((*vector_b)[n-1])/((*matrix)[n-1][n-1]);
-
 
 for(i=n-1; i>=0; i--){
     sum=0;
@@ -35,8 +45,26 @@ for(i=n-1; i>=0; i--){
     vector_x[i]=((*vector_b)[i]-sum)/(*matrix)[i][i];
 }
 
-
 for(i=0; i<3; i++){
     cout << vector_x[i] << endl;
 }
+}
+
+void gaussNoPivot(double (*matrix)[][3], double (*vector_b)[3], int n){
+
+int i, k, j;
+double m;
+
+
+for(k=0; k<n-1; k++){
+    for(i=k+1; i<n; i++){
+    m = (*matrix)[i][k] / (*matrix)[k][k];
+        for(j=k; j<n; j++){
+        (*matrix)[i][j] = (*matrix)[i][j]- m*(*matrix)[k][j];
+        } 
+        (*vector_b)[i] = (*vector_b)[i]-m*(*vector_b)[k];
+    }
+}
+
+inverseSub(matrix, vector_b, n);
 }
